755b9c11a408573b8fa2fdaff36b924da3220f45
Salih Onur Karakuş <salihonurkarakus98@gmail.com>
2025-11-26 18:00:26 +0300

    Add connection string

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -4,8 +4,14 @@ namespace dotnet_store.Models;
 
 public class DataContext : DbContext // DbContext sınıfının özelliklerini miras yolu ile DataContext içerisine aktarılıyor
 {
+    // yapıcı metotlar 
+    public DataContext(DbContextOptions<DataContext> options):base(options)
+    {
+        
+    }
     public DbSet<Product> Urunler { get; set; }
 }
 
 // Connection String
 //  Migrations
+
